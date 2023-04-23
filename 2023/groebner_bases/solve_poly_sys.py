11bad38dcd8ee3c01b61809475d3805fa3d33e5d
Jingnan Shi <jnshi@mit.edu>
2023-04-23 15:54:21 -0400

    working poly sys solver on example

--- 2023/groebner_bases/solve_poly_sys.py
+++ 2023/groebner_bases/solve_poly_sys.py
@@ -9,12 +9,78 @@ def is_univariate(f):
             return False
     return True
 
+def subs_root(f, gen, zero):
+    """ Substitute in a solution for a generator """
+    p = f.as_expr({gen: zero})
+
+    if f.degree(gen) >= 2:
+        p = p.expand(deep=False)
+
+    return p
+
+def solve_poly_system_recursive(F, gens, entry=False):
+    """ Recursive helper function 
+    Based on SymPy solve_generic
+    SymPy License: https://github.com/sympy/sympy/blob/master/LICENSE
+    """
+    basis = groebner(F, *gens)
+
+    if len(basis) == 1 and basis[0].is_ground:
+        if not entry:
+            return []
+        else:
+            return None
+
+    if len(basis) < len(gens):
+        raise ValueError("Potentially infinite solutions.")
+
+    univar = [x for x in basis if is_univariate(x)]
+
+    if len(univar) == 1:
+        f = univar.pop()
+    else:
+        raise ValueError("Potentially infinite solutions.")
+
+    # find solutions for the univariate element
+    gens = f.gens
+    gen = gens[-1]
+
+    zeros = list(sp.roots(f.ltrim(gen)).keys())
+
+    if not zeros:
+        # no solution
+        return []
+    
+    # single variable
+    if len(basis) == 1:
+        return [(zero,) for zero in zeros]
+
+    # recursively solve for the rest of the basis 
+    solutions = []
+    for zero in zeros:
+        new_system = []
+        new_gens = gens[:-1]
+
+        # back substitution of solution
+        for b in basis[:-1]:
+            eq = subs_root(b, gen, zero)
+
+            if eq is not sp.core.S.Zero:
+                new_system.append(eq)
+
+        new_system = sp.parallel_poly_from_expr(new_system, *new_gens)[0]
+        for solution in solve_poly_system_recursive(new_system, new_gens):
+            solutions.append(solution + (zero,))
+
+    return solutions
+
 
 def solve_poly_system(F, *gens):
-    """ Solve a system of polynomials with Groebner basis"""
-
-    G = groebner(F, *gens)
-    breakpoint()
+    """ Solve a system of polynomials with Groebner basis
+    Based on SymPy solve_generic
+    SymPy License: https://github.com/sympy/sympy/blob/master/LICENSE
+    """
+    result = solve_poly_system_recursive(F, gens, entry=True)
     
     return
 
@@ -26,8 +92,9 @@ if __name__ == "__main__":
     g_basis_sympy = sp.groebner(F, x, y, z, order="lex")
     print(f"Groebner's basis (SymPy): {g_basis_sympy}")
 
-    A = list(sp.ordered([x**3*y, x**2, x]))
-    B = list(sp.ordered([x, x**3*y, x**2]))
+    solution_sympy = sp.solve_poly_system(F, x, y, z)
+    print(f"Poly Sols (SymPy): {solution_sympy}")
+
     solve_poly_system(F, x, y, z)
 
 
